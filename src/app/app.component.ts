5abcbbfc6caa590a251c75382085f798dbbb982a
JoshiDev <joseluisjimenez1998@gmail.com>
2021-07-06 15:59:02 +0200

    Data biding y directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items=['nicolas', 'julian', 'perez'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/nivel-1.jpg',
+      title: 'Cuadrado Doble',
+      price: 8000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/nivel-2.jpg',
+      title: 'Libera la Anilla',
+      price: 8000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/nivel-3.jpg',
+      title: 'La T',
+      price: 8000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/nivel-4.jpg',
+      title: 'Acto de Equilibrio',
+      price: 8000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/nivel-5.jpg',
+      title: 'Saca el Palo',
+      price: 8000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/nivel-6.jpg',
+      title: 'Orejas de Burro',
+      price: 8000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addItem(){
+    this.items.push('nuevo item')
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1)
+  }
 }
